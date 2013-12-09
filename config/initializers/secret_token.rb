cb60b0cc400ac75161512119df6c5fd36b89a0d3
Darshan Bhatt <dbhatt@zappos.com>
2013-12-08 17:01:06 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grummmbler::Application.config.secret_key_base = 'e9761af5fdf920f64e973feeb87878f633685ca0dc747af241f2b1fcb7fcf915c5ebe22f8649992364661d1a42417c6109dac330a08d3bd9c28e17a02fe638c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grummmbler::Application.config.session_store :cookie_store, key: '_grummmbler_session'
